451e74f39d055aeac5c98659d7fe4bb1c906d1b6
Andre Anjos <andre.dos.anjos@gmail.com>
2021-10-22 08:56:23 +0200

    [recipe] Re-run autoconf machinery before build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,12 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
+libtoolize
+aclocal
+autoheader
+automake --add-missing
+autoreconf -fiv
+
 source activate "${CONDA_DEFAULT_ENV}"
 ./configure --prefix=$PREFIX --exec-prefix=$PREFIX
 make
