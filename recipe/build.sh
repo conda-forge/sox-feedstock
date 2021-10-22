d52c794a0348a1960913bb251847bf17dc95bbdb
Andre Anjos <andre.dos.anjos@gmail.com>
2021-10-22 13:20:22 +0200

    [recipe] Give-up trying to detect cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,10 +11,8 @@ autoreconf -fiv
 source activate "${CONDA_DEFAULT_ENV}"
 ./configure --prefix=$PREFIX --exec-prefix=$PREFIX
 make
-# only run tests during build phase if not cross-compiling
-if [[ "${HOST_PLATFORM}" == "${BUILD_PLATFORM}" ]]; then
-    echo "HOST_PLATFORM: ${HOST_PLATFORM}"
-    echo "BUILD_PLATFORM: ${BUILD_PLATFORM}"
+# only run tests during build phase if not cross-compiling for osx-arm64
+if [[ $(uname) != Darwin || $(uname -m) != arm64 ]]; then
     make bindir=. installcheck
 fi
 make install
