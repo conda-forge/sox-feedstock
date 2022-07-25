c49ed08c9a10b4e0d61c77c601517c4b729d3379
H. Vetinari <h.vetinari@gmx.com>
2022-07-25 22:26:35 +0200

    clean up build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env bash
+set -ex
 
-
-
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
-              -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
-              -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-               -DSOX_BUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=ON  .
-
+cmake -GNinja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+    -DCMAKE_INSTALL_INCLUDEDIR=${PREFIX}/include \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DSOX_BUILD_EXAMPLES=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    .
 
 cmake --build . --verbose --config Release -- -v -j ${CPU_COUNT}
 
 cmake --install . --verbose --config Release
-
-
